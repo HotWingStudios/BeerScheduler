5c5d34c13654b2accaaa028ad1a11642c038662e
jeffkodad <jkodad@dontpaniclabs.com>
2016-11-09 19:54:25 -0600

    Iteration0
    First commit

---
+++ BeerScheduler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeerScheduler.MvcApplication" Language="C#" %>
---
+++ BeerScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeerScheduler.Startup))]
+namespace BeerScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
